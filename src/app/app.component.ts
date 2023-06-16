42d007d121382ac1205b2da4ea06a2ade8843c92
GoktugYmsk <gogtug033@outlook.com>
2023-06-16 20:31:40 +0300

    descriptions added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-angular-project';
+  user = 'Göktuğ';
+
+  items = [
+    { description: "kahvaltı", action: "No" },
+    { description: "Sinema", action: "No" },
+    { description: "Kahve", action: "No" },
+    { description: "Ders", action: "No" },
+    { description: "Angular", action: "No" },
+  ];
+
 }
